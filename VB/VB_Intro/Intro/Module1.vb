da9268d38ba3927b28dbff63266f8b369f7d309e
a1c43my <sk8flowtheengineer@outlook.com>
2022-06-05 17:20:17 -0500

    vb updates

--- VB/VB_Intro/Intro/Module1.vb
+++ VB/VB_Intro/Intro/Module1.vb
@@ -1,7 +1,53 @@
 ﻿Module Module1
 
-    Sub Main()
 
+
+
+
+    ' my functions
+
+    Sub web(index, url)
+        Dim webClient As New System.Net.WebClient
+        Dim result As String = webClient.DownloadString(url)
+        Dim fs = CreateObject("Scripting.FileSystemObject")
+        Dim a = fs.CreateTextFile("~\Desktop\datatest" + index.ToString() + ".html", True)
+        a.WriteLine(result)
+        a.Close
+
+
+    End Sub
+
+    Sub CreateAfile(filename)
+        Dim fs = CreateObject("Scripting.FileSystemObject")
+        Dim a = fs.CreateTextFile("~\" + filename + ".txt", True)
+        a.WriteLine("This is a test.")
+        a.Close
+    End Sub
+
+    Sub CreateAfile2(data, options)
+
+        Dim fs = CreateObject("Scripting.FileSystemObject")
+        Dim a = fs.CreateTextFile("~\Desktop\data" + options + ".html", True)
+        a.WriteLine(data)
+        a.Close
+
+
+    End Sub
+
+    Sub makeone(data, option_)
+
+    End Sub
+    Sub Main()
+        Dim articles = {"http://localhost:4444/", "http://localhost/"}
+
+        For index = 0 To articles.GetUpperBound(0)
+            Dim curr = articles.ElementAt(index)
+            'Console.Write(index.ToString() + " : " + curr + vbNewLine)
+            web(index, curr)
+
+        Next
+        Console.WriteLine("Press Enter to finish" + vbNewLine)
+        Console.ReadKey()
     End Sub
 
 End Module
